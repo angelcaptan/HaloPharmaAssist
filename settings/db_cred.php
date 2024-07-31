6487bbe5b95a84a2764adc335902097c89521ea5
angelcaptan <122912829+angelcaptan@users.noreply.github.com>
2024-07-31 15:22:42 +0000

    Update db_cred.php
    Hosted Database Info

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,9 @@
 <?php
 
 //Database credentials
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+define("SERVER", "us-cluster-east-01.k8s.cleardb.net");
+define("USERNAME", "b309845316bbb7");
+define("PASSWD", "250ce10b");
 define("DATABASE","HaloPharma");
 
 ?>
