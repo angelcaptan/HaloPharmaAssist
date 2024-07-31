7e19c8f05e07f1659aec1a046ee7a65065673630
angelcaptan <122912829+angelcaptan@users.noreply.github.com>
2024-07-31 15:31:23 +0000

    Update db_cred.php
    proper db name

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -4,6 +4,6 @@
 define("SERVER", "us-cluster-east-01.k8s.cleardb.net");
 define("USERNAME", "b309845316bbb7");
 define("PASSWD", "250ce10b");
-define("DATABASE","HaloPharma");
+define("DATABASE","heroku_7c771ed5f9c1376");
 
 ?>
